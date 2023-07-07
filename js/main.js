7553a0969a64bde4e9a5bf407d23bc10264370da
AixaHernandezv <aixahernandezv@gmail.com>
2023-07-07 18:13:18 -0400

    cambio de boton modo oscuro

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-//seleccionamos todos los items del menú
+//selecciono todos los items del menú
 const menuItems = document.querySelectorAll('.menu-item');
-//Recorrer esa lista de nodos y detectar el click en cualquiera de ellos
+//Recorro la lista de nodos y detecto el click en cualquiera de ellos
 menuItems.forEach((item )=> {
     item.addEventListener('click', (e) =>{
        const currentItem = document.querySelector('.active');
@@ -15,22 +15,43 @@ menuItems.forEach((item )=> {
 //para esto debo seleccionar el icono y el menú
 
 const menuIcon = document.querySelector('#menuIcon');
-const navBarMobile = document.querySelector('#navBarMobile');
+const navBar = document.querySelector('#navBar'); 
 menuIcon.addEventListener('click', (e) =>{
-    /* const navbarMobileDisplayStatus = navBarMobile.style.display;
-    navBarMobile.style.display = (!navbarMobileDisplayStatus || navbarMobileDisplayStatus === 'none') 
-    ? 'flex' 
-    : 'none'; */
-    navBarMobile.classList.toggle('menu-mobile-active');
+    navBar.classList.toggle('menu-mobile-active');
 });
 
-const darkModeBtnElement = document.querySelector('#darkModeBtn');
+
 const bodyAllElement = document.querySelector('#bodyAll');
-const headerElement = document.querySelector('#header')
-darkModeBtnElement.addEventListener('click', (e)=> {
+const headerElement = document.querySelector('#header');
+const onSwitch = document.querySelector('#on');
+const offSwitch = document.querySelector('#off');
+const onLabel = document.querySelector("label[for='on']");
+const offLabel = document.querySelector("label[for='off']");
+const darkModeContainer = document.querySelector('#darkModeContainer');
 
-    bodyAllElement.classList.toggle('dark-mode-body');
-    headerElement.classList.toggle('dark-mode-body')
+onSwitch.addEventListener('click',(e)  => {
+        onSwitch.disabled = true;
+        offSwitch.disabled = false;
+        darkModeContainer.style.boxShadow = "inset -0.45em 0 0.45em #494949";
+        offLabel.style.color = "#8e8e8e";
+        onLabel.style.color = "#98f195";
+        bodyAllElement.classList.toggle('dark-mode-body');
+        headerElement.classList.toggle('dark-mode-body');
+});
 
+offSwitch.addEventListener('click',()  => {
+        onSwitch.disabled = false;
+        offSwitch.disabled = true;
+        darkModeContainer.style.boxShadow = "inset 0.45em 0 0.45em #171717";
+        onLabel.style.color = "#8e8e8e";
+        offLabel.style.color = "#f1959f";
+        bodyAllElement.classList.toggle('dark-mode-body');
+        headerElement.classList.toggle('dark-mode-body');
+});
 
+window.addEventListener('load',()=> {
+    offSwitch.disabled = true;
+    darkModeContainer.style.boxShadow = "inset 0.45em 0 0.45em #171717";
+    onLabel.style.color = "#8e8e8e";
+    offLabel.style.color = "#f1959f";
 })
